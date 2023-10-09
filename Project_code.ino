f829c62ab127f57a09a2c9e3a48262e5d546da32
pstockton3 <147439511+pstockton3@users.noreply.github.com>
2023-10-09 15:23:02 -0500

    Added project code
    This is the initial time uploading the code for the Arduino project.

---
+++ Project_code.ino
@@ -0,0 +1,22 @@
+/////////////////////////////////
+// LED_Blink
+//
+// This program blinks the LED #13
+//  off and on every 1 second
+/////////////////////////////////
+int ledPin = 13;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode (ledPin, OUTPUT); // Pin 13 is for LED 13
+
+}
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite (ledPin, HIGH);  // HIGH turns on LED
+  delay(1000); 
+  digitalWrite (ledPin, LOW);
+  delay(1000);
+  
+}
+
